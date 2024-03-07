f696b1b3ff79a4f7314dca29d37873bf5b0ea284
SammmmW <158096243+SammmmW@users.noreply.github.com>
2024-03-07 13:25:29 +0000

    Add files via upload

---
+++ TextReceiverSocket2.java
@@ -0,0 +1,101 @@
+/*
+ * TextReceiverSocket2.java
+ */
+
+/**
+ *
+ * @author  abj
+ */
+import java.net.*;
+import java.io.*;
+import java.nio.ByteBuffer;
+
+import CMPC3M06.AudioPlayer;
+
+
+public class TextReceiverSocket2 {
+
+    static DatagramSocket receiving_socket;
+
+    public static void main (String[] args) throws Exception{
+
+        //***************************************************
+        //Port to open socket on
+        int PORT = 55555;
+        //***************************************************
+
+        AudioPlayer player = new AudioPlayer();
+
+        //***************************************************
+        //Open a socket to receive from on port PORT
+
+        //DatagramSocket receiving_socket;
+        try{
+            receiving_socket = new DatagramSocket(PORT);
+        } catch (SocketException e){
+            System.out.println("ERROR: TextReceiver: Could not open UDP socket to receive from.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        //***************************************************
+
+        //***************************************************
+        //Main loop.
+
+        boolean running = true;
+
+        while (running){
+
+            try{
+                //Receive a DatagramPacket
+                byte[] buffer = new byte[514];
+                DatagramPacket packet = new DatagramPacket(buffer, 0, 514);
+                receiving_socket.setSoTimeout(33);
+
+                receiving_socket.receive(packet);
+
+                //get byte block from buffer
+                byte[] audio = buffer;
+                ByteBuffer unwrapDecrypt = ByteBuffer.allocate(buffer.length);
+
+                ByteBuffer cipherText = ByteBuffer.wrap(audio);
+                if (cipherText.getShort() == 10)
+                {
+                    int key = 1073948859;
+                    for (int j = 0; j < audio.length/4; j++)
+                    {
+                        int fourByte = cipherText.getInt();
+                        fourByte = fourByte ^ key;
+                        unwrapDecrypt.putInt(fourByte);
+                    }
+                    byte[] decryptedBlock = unwrapDecrypt.array();
+                    //muffle corrupted audio
+                    for (int i = 0; i < decryptedBlock.length; i++)
+                    {
+                        if (decryptedBlock[i] > 25 || decryptedBlock[i] < -25) //25 seems to be a sweet spot whereby it catches most of the corrupted clicking without losing too much of someone's real voice
+                        {
+                            decryptedBlock[i] = 0;
+                        }
+                    }
+
+                    //play it
+                    System.out.println("Playing received audio");
+                    player.playBlock(decryptedBlock);
+                }
+            }
+            catch (SocketTimeoutException e)
+            {
+                //on timeout play previously received packet
+                System.out.println(".");
+            }
+            catch (IOException e){
+                System.out.println("ERROR: TextReceiver: Some random IO error occured!");
+                e.printStackTrace();
+            }
+        }
+        //Close the socket
+        receiving_socket.close();
+        player.close();
+        //***************************************************
+    }
+}
---
+++ TextSenderSocket2.java
@@ -0,0 +1,105 @@
+/*
+ * TextSenderSocket2.java
+ */
+
+/**
+ *
+ * @author  abj
+ */
+import java.net.*;
+import java.io.*;
+import java.nio.ByteBuffer;
+
+import CMPC3M06.AudioRecorder;
+import uk.ac.uea.cmp.voip.*;
+
+
+public class TextSenderSocket2{
+
+    static DatagramSocket4 sending_socket;
+
+    public static void main (String[] args) throws Exception{
+
+        //***************************************************
+        //Port to send to
+        int PORT = 55555;
+        //IP ADDRESS to send to
+        InetAddress clientIP = null;
+
+        AudioRecorder recorder = new AudioRecorder();
+
+        //Recording time in seconds
+        int recordTime = 10;
+        try {
+            clientIP = InetAddress.getByName("localhost");
+        } catch (UnknownHostException e) {
+            System.out.println("ERROR: TextSender: Could not find client IP");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        //***************************************************
+
+        //***************************************************
+        //Open a socket to send from
+        //We dont need to know its port number as we never send anything to it.
+        //We need the try and catch block to make sure no errors occur.
+
+        //DatagramSocket sending_socket;
+        try{
+            sending_socket = new DatagramSocket4();
+        } catch (SocketException e){
+            System.out.println("ERROR: TextSender: Could not open UDP socket to send from.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        //***************************************************
+
+        //***************************************************
+        //Get a handle to the Standard Input (console) so we can read user input
+
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        //***************************************************
+
+        //***************************************************
+        //Main loop.
+
+//        boolean running = true;
+
+        System.out.println("Recording audio...");
+        int key = 1073948859; //key should be between 2^30 and 2^31 to cover all bits
+        short authenticationKey = 10; //authentication key must be a short so receiver can remove it from packet
+
+        for (int i = 0; i < Math.ceil(recordTime / 0.032); i++) {
+            try{
+
+                byte[] buffer = recorder.getBlock();
+                ByteBuffer unwrapEncrypt = ByteBuffer.allocate(514);
+                unwrapEncrypt.putShort(authenticationKey);
+
+                ByteBuffer plainText = ByteBuffer.wrap(buffer);
+                for (int j = 0; j < buffer.length/4; j++)
+                {
+                    int fourByte = plainText.getInt();
+                    fourByte = fourByte ^ key;
+                    unwrapEncrypt.putInt(fourByte);
+                }
+                byte[] encryptedBlock = unwrapEncrypt.array();
+
+                //Make a DatagramPacket from it, with client address and port number
+                DatagramPacket packet = new DatagramPacket(encryptedBlock, encryptedBlock.length, clientIP, PORT);
+
+                //Send it
+                sending_socket.send(packet);
+
+            } catch (IOException e){
+                System.out.println("ERROR: TextSender: Some random IO error occured!");
+                e.printStackTrace();
+            }
+        }
+        //Close the socket
+        sending_socket.close();
+        System.out.println("Packets sent");
+        recorder.close();
+        //***************************************************
+    }
+} 
